4a3a5fb7bdf3e77a18d8697c3f5002ed3a699994
Elle Harding <ehardin0@chicagobooth.edu>
2023-04-17 18:15:36 +0000

    Selection Page Commit

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -12,6 +12,24 @@ class CurrencyController < ApplicationController
   end
 
   def convert_currency
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+
+    @array_of_currencies = @symbols_hash.keys
+
+    #params are
+    @from_currency = params.fetch("from_currency")
+
+    # @raw_data = open("https://api.exchangerate.host/convert?from=USD&to=EUR").read
+    # @parsed_data = JSON.parse(@raw_data)
+    # @symbols_hash = @parsed_data.fetch("symbols")
+
+
+    # @array_of_currencies = @symbols_hash.keys
     render({ :template => "currency_templates/conversion.html.erb"})
   end
+
 end
